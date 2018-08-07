88bfe42fdf19567e4879f62eca7e307b005acc71
Luiz Cunha <luiz.cunha@projetas.com.br>
2018-08-07 19:53:19 -0300

    commit

--- src/main/java/com/projetocoach/controller/VeiculoController.java
+++ src/main/java/com/projetocoach/controller/VeiculoController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class VeiculoController {
 
-    @RequestMapping("/")
-    public String index(){
-        return "index";
+    @RequestMapping("/cadastro")
+    private String form(){
+        return "veiculo/formVeiculo";
     }
 
 }
---
+++ src/main/java/com/projetocoach/models/Veiculo.java
@@ -0,0 +1,77 @@
+package com.projetocoach.models;
+
+public class Veiculo {
+
+    private int id;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private String ano;
+    private String preco;
+    private String descricao;
+    private Boolean tipo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Boolean tipo) {
+        this.tipo = tipo;
+    }
+}
