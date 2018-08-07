24528bcdea36fc7d6512ff813922b4f12c0ad4c2
Luiz Cunha <luiz.cunha@projetas.com.br>
2018-08-07 19:21:00 -0300

    first commit

---
+++ src/main/java/com/projetocoach/ProjetoCoachApplication.java
@@ -0,0 +1,12 @@
+package com.projetocoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCoachApplication.class, args);
+	}
+}
