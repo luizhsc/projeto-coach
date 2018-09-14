a24aef4ce273a0b69b2b21f958118358c08c9802
Luiz Cunha <luiz.cunha@projetas.com.br>
2018-09-14 17:49:14 -0300

    commit

--- src/main/java/com/projetocoach/service/VeiculoService.java
+++ src/main/java/com/projetocoach/service/VeiculoService.java
@@ -1,21 +1,20 @@
 package com.projetocoach.service;
 
 
-import com.projetocoach.model.Veiculo;
 import com.projetocoach.service.dto.VeiculoDTO;
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface VeiculoService {
     Page<VeiculoDTO> findAll(Pageable pageable);
 
-    VeiculoDTO findById(Long id);
+    Optional<VeiculoDTO> findById(Long id);
 
     VeiculoDTO save(VeiculoDTO veiculoDTO);
 
     VeiculoDTO update(VeiculoDTO veiculoDTO);
 
-    void delete(long id);
+    void delete(Long id);
 
 }
--- src/main/java/com/projetocoach/service/impl/VeiculoServiceImpl.java
+++ src/main/java/com/projetocoach/service/impl/VeiculoServiceImpl.java
@@ -1,50 +1,51 @@
 package com.projetocoach.service.impl;
 
 import com.projetocoach.mapper.VeiculoMapper;
+import com.projetocoach.model.Veiculo;
+import com.projetocoach.repository.VeiculoRepository;
 import com.projetocoach.service.VeiculoService;
 import com.projetocoach.service.dto.VeiculoDTO;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public class VeiculoServiceImpl implements VeiculoService {
 
-    private final VeiculoService veiculoService;
+    private final VeiculoRepository veiculoRepository;
 
     private final VeiculoMapper mapper;
 
     @Autowired
-    public VeiculoServiceImpl(VeiculoService veiculoService, VeiculoMapper mapper) {
-        this.veiculoService = veiculoService;
+    public VeiculoServiceImpl(VeiculoRepository veiculoRepository, VeiculoMapper mapper) {
+        this.veiculoRepository = veiculoRepository;
         this.mapper = mapper;
     }
 
     @Override
     public Page<VeiculoDTO> findAll(Pageable pageable) {
-        return veiculoService.findAll(pageable);
+        return veiculoRepository.findAll(pageable).map(mapper::toDto);
     }
 
     @Override
-    public VeiculoDTO findById(Long id) {
+    public Optional<VeiculoDTO> findById(Long id) {
         //TODO: create specific exception / user mapper
-        return veiculoService.findById(id);
+        return veiculoRepository.findById(id).map(mapper::toDto);
+    }
+
+    public Veiculo save(Veiculo veiculo) {
+        return veiculoRepository.save(veiculo);
     }
 
     @Override
-    public VeiculoDTO save(VeiculoDTO veiculoDTO) {
+    public Veiculo update(Veiculo veiculo) {
         //TODO: use mapper
-        return veiculoService.save(veiculoDTO);
+        return veiculoRepository.save(veiculo);
     }
 
     @Override
-    public VeiculoDTO update(VeiculoDTO veiculoDTO) {
-        //TODO: use mapper
-        return veiculoService.save(veiculoDTO);
-    }
-
-    @Override
-    public void delete(long id) {
-        veiculoService.delete(id);
+    public void delete(Long id) {
+        return veiculoRepository.deleteById(id);
     }
 
 }
