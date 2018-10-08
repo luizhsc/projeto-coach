522044c6c6d8c7e21fb3602463049bb774aae5f0
Luiz Cunha <luiz.cunha@projetas.com.br>
2018-10-08 17:01:46 -0300

    melhoria teste

--- src/test/java/com/projetocoach/service/impl/VeiculoServiceImplTest.java
+++ src/test/java/com/projetocoach/service/impl/VeiculoServiceImplTest.java
@@ -1,34 +1,25 @@
 package com.projetocoach.service.impl;
 
-import com.projetocoach.mapper.VeiculoMapperImpl;
-import com.projetocoach.model.Veiculo;
-import com.projetocoach.repository.VeiculoRepository;
 import com.projetocoach.service.VeiculoService;
 import com.projetocoach.service.dto.VeiculoDTO;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-@TestPropertySource({"/application-test.properties"})
 public class VeiculoServiceImplTest{
 
     @Test
     public void buscarTodosVeiculos() {
         VeiculoService veiculoService = Mockito.mock(VeiculoService.class);
-        VeiculoRepository veiculoRepository = Mockito.mock(VeiculoRepository.class);
-        VeiculoMapperImpl veiculoMapper = Mockito.mock(VeiculoMapperImpl.class);
         Pageable pageable = Mockito.mock(Pageable.class);
+        Page page = Mockito.mock(Page.class);
 
         VeiculoDTO veiculoDTO = new VeiculoDTO();
 
@@ -36,18 +27,14 @@ public class VeiculoServiceImplTest{
         veiculoDTO.setModelo("Teste");
         veiculoDTO.setAno("2000");
 
-        List<Veiculo> veiculo = new ArrayList<>();
+        Mockito.when(veiculoService.findAll(pageable)).thenReturn(page);
 
-        Page<Veiculo> page = new PageImpl<>(veiculo);
-
-        Mockito.when(veiculoRepository.findAll()).thenReturn(veiculo, (List<Veiculo>) page);
-
-        Assert.assertEquals(page.map(veiculoMapper::toDto), veiculoService.findAll(pageable));
+        Assert.assertEquals(veiculoService.findAll(pageable), page);
     }
 
 
     @Test
-    public void buscaPorId(){
+    public void buscarVeiculoPorId(){
 
     }
 
