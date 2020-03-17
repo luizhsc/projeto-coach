b1445d0166f2eec255ee982f67f25348993aa12d
Luiz Silva Cunha <luiz.silva.cunha@everis.com>
2020-03-17 15:54:47 -0300

    altera controller

--- src/main/java/com/projetocoach/controller/VeiculoController.java
+++ src/main/java/com/projetocoach/controller/VeiculoController.java
@@ -3,28 +3,24 @@ package com.projetocoach.controller;
 import com.projetocoach.dto.VeiculoDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import java.net.URISyntaxException;
 
 @Component public interface VeiculoController {
 
-    @GetMapping @Consumes(MediaType.APPLICATION_JSON) @Produces(MediaType.APPLICATION_JSON)
-    public Page<VeiculoDto> getAll(Pageable pageable);
+    @GetMapping ResponseEntity<Page<VeiculoDto>> getAll(Pageable pageable);
 
-    @GetMapping("/{id}") @Consumes(MediaType.APPLICATION_JSON) @Produces(MediaType.APPLICATION_JSON)
-    public VeiculoDto getById(@PathVariable("id") Long id);
+    @GetMapping("/{id}") @ResponseBody VeiculoDto getById(@PathVariable("id") Long id);
 
-    @PostMapping @Consumes(MediaType.APPLICATION_JSON) @Produces(MediaType.APPLICATION_JSON)
-    public VeiculoDto create(@Valid @RequestBody VeiculoDto veiculoDto);
+    @PostMapping ResponseEntity<VeiculoDto> create(@Valid @RequestBody VeiculoDto veiculoDto)
+        throws URISyntaxException;
 
-    @PutMapping("/{id}") @Consumes(MediaType.APPLICATION_JSON) @Produces(MediaType.APPLICATION_JSON)
-    public VeiculoDto update(@RequestBody VeiculoDto veiculoDto);
+    @PutMapping("/{id}") ResponseEntity<VeiculoDto> update(@RequestBody VeiculoDto veiculoDto);
 
-    @DeleteMapping("/{id}") @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON) public void delete(@PathVariable("id") Long id);
+    @DeleteMapping("/{id}") public ResponseEntity delete(@PathVariable("id") Long id);
+
 }
--- src/main/java/com/projetocoach/controller/VeiculoControllerImpl.java
+++ src/main/java/com/projetocoach/controller/VeiculoControllerImpl.java
@@ -6,33 +6,37 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Api(value = "Veiculos API REST") @RestController @RequestMapping("veiculos")
 public class VeiculoControllerImpl implements VeiculoController {
 
     @Autowired private VeiculoService service;
 
-    @Override public Page<VeiculoDto> getAll(Pageable pageable) {
-        return service.findAll(pageable);
+    public ResponseEntity<Page<VeiculoDto>> getAll(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
     }
 
-    @Override public VeiculoDto getById(Long id) {
+    public VeiculoDto getById(Long id) {
         return service.findById(id);
     }
 
-    @Override public VeiculoDto create(VeiculoDto veiculoDto) {
-        return service.save(veiculoDto);
+    public ResponseEntity<VeiculoDto> create(VeiculoDto veiculoDto) throws URISyntaxException {
+        return ResponseEntity.created(new URI("/veiculos/" + service.save(veiculoDto))).build();
     }
 
-    @Override public VeiculoDto update(@RequestBody VeiculoDto veiculoDto) {
-        return service.update(veiculoDto);
+    public ResponseEntity<VeiculoDto> update(VeiculoDto veiculoDto) {
+        return ResponseEntity.ok(service.update(veiculoDto));
     }
 
-    @Override public void delete(@PathVariable("id") Long id) {
+    public ResponseEntity delete(Long id) {
         service.deleteById(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
